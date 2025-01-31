b3d18383ecc82f04611753edd91574ab43c544ee
ERI SAKAMOTO <202411ap01_sakamoto@x-ide.sakura.ne.jp>
2025-01-31 13:59:35 +0900

    #34517
    業者管理_テスト仕様書バグNo.
    
    ■修正内容
    chromeのブラウザ側の翻訳のポップアップが中国語と認識してしまっていたため、
    lang=jaを入れて回避

--- SRC/index.php
+++ SRC/index.php
@@ -68,7 +68,7 @@ if ($_COOKIE['cUserNo'] != '' && $_COOKIE['authority'] != '') {
 }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<html lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="./css/style.css" />
