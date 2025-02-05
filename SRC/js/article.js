83e1dfc8395e6f9a142379df1f0bab9171c779ea
ERI SAKAMOTO <202411ap01_sakamoto@x-ide.sakura.ne.jp>
2025-02-05 13:57:38 +0900

    #34685
    物件管理_テスト仕様書バグNo.115、117、119、121、123、125、127
    
    ■修正内容
    バリデーションで文字数制限が書かれていなかったため追加

--- SRC/js/article.js
+++ SRC/js/article.js
@@ -12,6 +12,48 @@ function fnArticleEditCheck() {
 		return;
 	}
 
+	tmp = form.room.value;
+	if (tmp.length > 100) {
+		alert('部屋番号は100文字以内で入力してください');
+		return;
+	}
+	
+	tmp = form.keyPlace.value;
+	if (tmp.length > 200) {
+		alert('鍵場所は100文字以内で入力してください');
+		return;
+	}
+
+	tmp = form.address.value;
+	if (tmp.length > 100) {
+		alert('住所は100文字以内で入力してください');
+		return;
+	}
+
+	tmp = form.articleNote.value;
+	if (tmp.length > 200) {
+		alert('備考は200文字以内で入力してください');
+		return;
+	}
+
+	tmp = form.keyBox.value;
+	if (tmp.length > 100) {
+		alert('キーBox番号は100文字以内で入力してください');
+		return;
+	}
+
+	tmp = form.drawing.value;
+	if (tmp.length > 100) {
+		alert('3Dパースは100文字以内で入力してください');
+		return;
+	}
+
+	tmp = form.sellCharge.value;
+	if (tmp.length > 100) {
+		alert('営業担当は100文字以内で入力してください');
+		return;
+	}
+
 	if (confirm('この内容で登録します。よろしいですか？')) {
 		form.act.value = 'articleEditComplete';
 		form.submit();
