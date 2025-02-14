fddd8478fd8f36496a82234fbe84f4ac425a7d33
ERI SAKAMOTO <202411ap01_sakamoto@x-ide.sakura.ne.jp>
2025-02-14 15:00:34 +0900

    物件管理_テスト仕様書バグNo.313
    ■修正箇所
    DBで更新日時がアップデートされない

--- SRC/libDBConst.php
+++ SRC/libDBConst.php
@@ -180,6 +180,7 @@ function fnSqlConstUpdate(
 	$sql .= ",ELECTRICITYCHARGE = '$electricityCharge'";
 	$sql .= ",GASCHARGE = '$gasCharge'";
 	$sql .= ",LIGHTORDER = '$lightOrder'";
+    $sql .= ",UPDT = CURRENT_TIMESTAMP"; // 更新日時を追加
 	$sql .= " WHERE ARTICLENO = $articleNo";
 
 	return ($sql);
