ffdf7deb6fb1b41e589dfab90c5df3403385c031
ERI SAKAMOTO <202411ap01_sakamoto@x-ide.sakura.ne.jp>
2025-02-19 11:36:16 +0900

    #35888
    売主物件_テスト仕様書バグNo.191
    
    ■修正箇所
    年だけ5桁以上を受け付けるため修正

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -5,26 +5,47 @@
  * @return true:正常、false:異常
  */
 function fnYMDCheck(msg, obj) {
-	// 未入力時はチェックしない
-	oYMD = obj.value;
+    // 未入力時はチェックしない
+    oYMD = obj.value;
 
-	if (!oYMD) {
-		return true;
-	}
+    if (!oYMD) {
+        return true;
+    }
 
-	var tmp = oYMD.split('/');
-	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
-	var vYMD = ymd.getFullYear() + '/' + ('0' + (ymd.getMonth() + 1)).slice(-2) + '/' + ('0' + ymd.getDate()).slice(-2);
-	if (oYMD == vYMD) {
-		return true;
-	} else {
-		alert(msg + "を入力してください");
-		return false;
-	}
+    // 日付フォーマットのチェック（yyyy/mm/dd）
+    if (!oYMD.match(/^\d{4}\/\d{2}\/\d{2}$/)) {
+        alert(msg + "を入力してください");
+        return false;
+    }
+
+    var tmp = oYMD.split('/');
+    
+    // 年・月・日が適切な範囲かチェック
+    var year = parseInt(tmp[0], 10);
+    var month = parseInt(tmp[1], 10);
+    var day = parseInt(tmp[2], 10);
+    
+    if (year < 1900 || year > 2100 || month < 1 || month > 12 || day < 1 || day > 31) {
+        alert(msg + "を入力してください");
+        return false;
+    }
+
+    var ymd = new Date(year, month - 1, day);
+    
+    // 実際に存在する日付かチェック（例：2月30日などの無効な日付を検出）
+    if (ymd.getFullYear() !== year || ymd.getMonth() !== month - 1 || ymd.getDate() !== day) {
+        alert(msg + "を入力してください");
+        return false;
+    }
+
+    var vYMD = ymd.getFullYear() + '/' + ('0' + (ymd.getMonth() + 1)).slice(-2) + '/' + ('0' + ymd.getDate()).slice(-2);
+    if (oYMD === vYMD) {
+        return true;
+    } else {
+        alert(msg + "を入力してください");
+        return false;
+    }
 }
-
-
-
 /**
  * 入力桁数チェック
  *
