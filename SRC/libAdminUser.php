e260ced5697d775d5d7ca05d998d087542c6af45
ERI SAKAMOTO <202411ap01_sakamoto@x-ide.sakura.ne.jp>
2025-03-07 12:05:44 +0900

    #36879
    ユーザー情報_テスト仕様書バグNo.94
    
    ■バグ箇所
    パスワードがそのまま表示されている

--- SRC/libAdminUser.php
+++ SRC/libAdminUser.php
@@ -41,7 +41,15 @@ function subAdminUser()
 					<tr>
 						<td class="list_td<?php print $i; ?>"><a href="javascript:form.act.value='adminUserEdit';form.userNo.value=<?php print $userNo; ?>;form.submit();"><?php print $name; ?></a></td>
 						<td class="list_td<?php print $i; ?>"><?php print $id; ?></td>
-						<td class="list_td<?php print $i; ?>"><?php print $password; ?></td>
+						<td class="list_td<?php print $i; ?>">
+							<?php 
+							$pass_count = mb_strlen($password);
+							while($i < $pass_count){
+								echo '*';
+								$i+=1;
+							}
+							?>
+						</td>
 						<td class="list_td<?php print $i; ?>"><?php print fnAuthorityName($authority); ?></td>
 						<td class="list_td<?php print $i; ?>" align="center">
 							<?php if($userNo == 1){?>
@@ -111,7 +119,7 @@ function subAdminUserEdit()
 			</tr>
 			<tr>
 				<th>PASS<span class="red">（必須）</span></th>
-				<td><input type="text" name="password" value="<?php print $password; ?>" /></td>
+				<td><input type="password" name="password" value="<?php print $password; ?>" /></td>
 			</tr>
 			<tr>
 				<th>所属</th>
